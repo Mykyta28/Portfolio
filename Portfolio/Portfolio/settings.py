ba1df7dcadbad3067ee2d9d43d21eeee3259c4b5
Nikita Semenii <115111003+Mykyta28@users.noreply.github.com>
2026-08-21 10:47:53 -0500

    Add email timeout setting to Django settings

--- Portfolio/Portfolio/settings.py
+++ Portfolio/Portfolio/settings.py
@@ -128,6 +128,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_TIMEOUT = 5
 
 EMAIL_HOST_USER = 'semnik280997@gmail.com'
 EMAIL_HOST_PASSWORD = 'REMOVED_PASSWORD'
